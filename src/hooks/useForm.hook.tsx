7786b80e473f3977016217bf3a92a876040b290f
Jesus Puentes <jopt98@gmail.com>
2025-12-24 22:52:44 -0600

    Improved structure of useform

--- src/hooks/useForm.hook.tsx
+++ src/hooks/useForm.hook.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react"
 
+
 const useForm = <T extends object>(initialState: T) => {
     const [form, setForm] = useState<T>(initialState)
-    const [formErrors, setFormErrors] = useState<{[key: string]: boolean}>({});
+    const [formErrors, setFormErrors] = useState<Record<keyof T, boolean>>(
+        Object.keys(initialState).reduce((acc, key) => {
+            acc[key as keyof T] = false;
+            return acc;
+        }, {} as Record<keyof T, boolean>)
+    );
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setForm((prev) => ({
@@ -13,12 +19,17 @@ const useForm = <T extends object>(initialState: T) => {
 
     const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
         const name = e.target.name;
-        form[name as keyof T] === '' ? setFormErrors({...formErrors, [name]: true}) : setFormErrors({...formErrors, [name]: false}) 
+        form[name as keyof T] === '' ? setFormErrors((prev) => ({...prev, [name]: true})) : setFormErrors((prev) => ({...prev, [name]: false})) 
     }
 
     const resetForm = () => {
         setForm(initialState);
-        setFormErrors({});
+        setFormErrors(
+            Object.keys(initialState).reduce((acc, key) => {
+                acc[key as keyof T] = false;
+                return acc;
+            }, {} as Record<keyof T, boolean>)
+        );
     }
 
     return {
